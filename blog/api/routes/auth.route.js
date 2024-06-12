602799e1efcf66583f428dbf43120ab7beb9e5c9
Bichu-B-Binu <bichubbinu77@gmail.com>
2024-06-12 15:21:12 +0530

    create sigin in api route

--- blog/api/routes/auth.route.js
+++ blog/api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.controler.js";
+import { signin, signup } from "../controllers/auth.controler.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
+router.post("/signUp", signup);
+router.post("/signIn", signin);
 
 export default router;
