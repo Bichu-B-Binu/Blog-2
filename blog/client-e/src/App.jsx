491151c5c6891e786bfea509c490becfc8a30e93
Bichu-B-Binu <bichubbinu77@gmail.com>
2024-06-26 17:23:19 +0530

    add update post functionality

--- blog/client-e/src/App.jsx
+++ blog/client-e/src/App.jsx
@@ -11,6 +11,7 @@ import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
 
 function App() {
   return (
@@ -23,7 +24,8 @@ function App() {
           <Route path="/signIn" element={<SignIn />} />
           <Route path="/signUp" element={<SignUp />} />
           <Route element={<PrivateRoute />}>
-            <Route path="/dashboard" element={<CreatePost />} />
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/update-post/:postId" element={<UpdatePost />} />
           </Route>
 
           <Route element={<OnlyAdminPrivateRoute />}>
